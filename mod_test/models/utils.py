80612b157757a90c62488a1875c8f5369857f9fc
LeGrandKantin <fomekongorich@yahoo.com>
2023-02-12 21:53:42 -0800

    free space

---
+++ mod_test/models/utils.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import joblib
+
+
+def get_excel_file(file_name):
+    return pd.read_excel(f"./mod_test/static/documents/{file_name}.xlsx")
+
+def get_model(file_name):
+    return joblib.load(f"./mod_test/static/documents/{file_name}.pkl")
+     
+
+def convert_df_html(result):
+    return [result.to_html(classes="data", header=True)]
